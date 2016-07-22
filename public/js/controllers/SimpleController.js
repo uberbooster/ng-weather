01fe963238439ff89fc232eaec2e9805300b166b
UberBooster <info@uberbooster.com>
2016-07-21 20:50:04 -0400

    now grabs minutely and daily weather from the service

--- public/js/controllers/SimpleController.js
+++ public/js/controllers/SimpleController.js
@@ -7,6 +7,8 @@ SimpleControllerF.$inject = ['$scope', 'WeatherService'];
 function SimpleControllerF($scope, WeatherService){
   $scope.message = "Hello There!";
   WeatherService.getHourlyData(29, -82);
+  WeatherService.getMinutelyData(29, -82);
+  WeatherService.getDailyData(29, -82);
 };
 
 })();
--- public/js/services/WeatherService.js
+++ public/js/services/WeatherService.js
@@ -7,6 +7,7 @@ WeatherServiceF.$inject = ['$http'];
 function WeatherServiceF($http){
   var passphrase = 'i like cheese and bacon and some other things too';
   var baseUrl = 'https://quiet-bayou-88937.herokuapp.com/';
+  var config = {headers: {'passphrase': passphrase}};
   var service = {
     getHourlyData: getHourlyDataF,
     getMinutelyData: getMinutelyDataF,
@@ -17,20 +18,32 @@ function WeatherServiceF($http){
 
   function getHourlyDataF(lat, lon){
     var url = baseUrl + 'forecast/hourly/' + lat + ',' + lon;
-    console.log(service.weather);
-    var config = {
-      headers: {
-        'passphrase': passphrase
-      }
-};
+    console.log("Hourly: ",service.weather);
     return $http.get(url, config)
                 .then(function(response){
                   service.weather = response.data;
-                  console.log(service.weather);
+                  console.log("Hourly: ",service.weather);
                 });
   };
 
-  function getMinutelyDataF(lat, lon){}
-  function getDailyDataF(lat, lon){}
-}
+  function getMinutelyDataF(lat, lon){
+    var url = baseUrl + 'forecast/minutely/' + lat + ',' + lon;
+    console.log("Minutely: ",service.weather);
+    return $http.get(url, config)
+                .then(function(response){
+                  service.weather = response.data;
+                  console.log("Minutely: ",service.weather);
+                });
+  };
+
+  function getDailyDataF(lat, lon){
+    var url = baseUrl +'forecast/daily/' + lat + ',' + lon;
+    console.log("Daily: ",service.weather);
+    return $http.get(url, config)
+                .then(function(response){
+                  service.weather = response.data;
+                  console.log("Daily: ",service.weather);
+                });
+  };
+};
 })();
