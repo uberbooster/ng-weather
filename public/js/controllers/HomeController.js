37994f1ea2224e0092e7f4f01f653b9377c7b2d9
UberBooster <info@uberbooster.com>
2016-07-22 21:07:09 -0400

    implemented hourly transition

--- public/js/controllers/HomeController.js
+++ public/js/controllers/HomeController.js
@@ -2,9 +2,18 @@
   angular.module('ngWeather')
          .controller('HomeController', HomeControllerF);
 
-  HomeControllerF.$inject = [];
+  HomeControllerF.$inject = ['$scope', 'WeatherService'];
 
-  function HomeControllerF(){
-    
+  function HomeControllerF($scope, WeatherService){
+    $scope.updateHourly = updateHourlyF;
+    $scope.updateMinutely = updateMinutelyF;
+    $scope.updateDaily = updateDailyF;
+
+    function updateHourlyF(latitude, longitude){
+      console.log("latitude: ",latitude, " Longitude: ", longitude);
+      WeatherService.getHourlyData(latitude, longitude);
+    }
+    function updateMinutelyF(latitude, longitude){}
+    function updateDailyF(latitude, longitude){}
   }
 })();
