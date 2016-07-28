a74b14f9d82ce0997ca9b29a1f14bfc1a5bfd9ea
UberBooster <info@uberbooster.com>
2016-07-28 19:24:32 -0400

    google now converts city data to lat lon data for hourly view only

--- public/js/services/GeoLocation.js
+++ public/js/services/GeoLocation.js
@@ -2,17 +2,26 @@
   angular.module('ngWeather')
          .factory('GeoLocation', GeoLocationF);
 
-GeoLocationF.$inject = ['$http', '$scope'];
+GeoLocationF.$inject = ['$http'];
 
-function GeoLocationF($http, $scope){
+function GeoLocationF($http){
   var baseUrlGoogleLocationToCoordinates = 'https://maps.googleapis.com/maps/api/geocode/json?address=';
-  $http.get(baseUrlGoogleLocationToCoordinates + $scope.location)
-    .then(function(response){
-      $scope.geoInfo = response.data;
-      console.log("$scope.geoInfo: ",$scope.geoInfo);
-    });
+
+  var service = {
+    getLocationData: getLocationDataF,
+    formattedAddress: ''
+  };
+  return service;
+
+  function getLocationDataF(location){
+    return $http.get(baseUrlGoogleLocationToCoordinates + location)
+      .then(function(response){
+        console.log('GeoLocation Response:', response)
+        service.formattedAddress = response.data.results[0].formatted_address;
+        return response.data.results[0].geometry.location;
+      });
+  }
+
 };
 
-
-
-}();
+})();
